740a7061a12bdf8033a4ddc93380119fa308aad3
Jeffrey <jeffrey.popek@gmail.com>
2018-07-19 13:24:55 -0700

    commit die

--- core/src/com/mygdx/game/States/RoomState.java
+++ core/src/com/mygdx/game/States/RoomState.java
@@ -1,6 +1,37 @@
 package com.mygdx.game.States;
 
-public class RoomState {
-    public RoomState() {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.TheExcellentDucks;
+
+public class RoomState extends State{
+
+    Texture bg;
+
+    public RoomState(GameStateManager stateManager) {
+        super(stateManager);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(bg, 0, 0, TheExcellentDucks.WIDTH, TheExcellentDucks.HEIGHT);
+        sb.end();
+
+    }
+
+    @Override
+    public void dispose() {
+
     }
 }
